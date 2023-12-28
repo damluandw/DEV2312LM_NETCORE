7afa5d6e2dcf5c5dcb1a1c039d603f8069af0870
damluandw <damluan7@gmail.com>
2023-12-29 00:03:03 +0700

    update Lesson01

---
+++ Lesson01/Lab1_2_P2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab1_2_P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {      
+            string maNV = "NV01";
+            string ten = "Dam Luan";
+            string ngaySinh = "17/09/1997";
+            string diaChi = "351 Au Co";
+            string sdt = "0976897563";
+            string email = "damluan7@gmail.com";
+            Console.WriteLine("Ma NV: {0}\nTen: {1}\nNgay sinh {2}\nDia chi: {3}\nSo dien thoai: {4}\nEmail: {5}", maNV,ten,ngaySinh,diaChi,sdt,email);
+        }
+    }
+}
---
+++ Lesson01/Lab2_3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
