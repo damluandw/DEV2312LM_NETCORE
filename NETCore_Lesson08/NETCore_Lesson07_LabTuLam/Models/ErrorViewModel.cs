9bdd52f81bfc5c436a20904e5879ce4026d330f7
damluandw <damluan7@gmail.com>
2024-03-18 21:27:27 +0700

    add lesson08

---
+++ NETCore_Lesson08/NETCore_Lesson07_LabTuLam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NETCore_Lesson07_LabTuLam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
